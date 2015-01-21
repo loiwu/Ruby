74433d99abe18561df6c066458d255396c96c3ae
LoiWu <superloiwu@gmail.com>
2015-01-21 16:55:44 +0800

    Rails (Starter)
    [1/21/15, 16:11:44] loi wu: curl -L https://get.rvm.io | bash -s stable
    [1/21/15, 16:14:44] loi wu: rvm install 2.0.0 --autolibs=enable
    [1/21/15, 16:15:36] loi wu: rvm use 2.0.0
    [1/21/15, 16:16:39] loi wu: gem install rails --version 4.0.0 --no-ri
    --no-rdoc
    [1/21/15, 16:17:42] loi wu:  $ rvm --default 2.0.0
    [1/21/15, 16:17:47] loi wu: $ rails -v
    [1/21/15, 16:18:02] loi wu: gem list --local rails
    [1/21/15, 16:18:20] loi wu: $ rails 4.0.0 --version
    [1/21/15, 16:32:58] loi wu: cd BitBub/WordPress/Ruby/Rails/work
    [1/21/15, 16:37:27] loi wu: rails new demo
    [1/21/15, 16:55:01] loi wu: cd demo
    [1/21/15, 16:55:07] loi wu: rake about
    [1/21/15, 16:55:17] loi wu: rails server
    [1/21/15, 16:55:21] loi wu: rails server webrick

---
+++ Rails/work/demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'df27f8b06c001a386070091a96aac13e437d052a821511a9ea005480a82d11ee826694247f70a75f50d441d5fd3fe738d3b2df4636f9a2d24ecc003fc6b07a78'
